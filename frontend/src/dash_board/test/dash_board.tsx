0577b06cb0476768b4326eacf5ec7a0a7c5c2adc
GarF1eldGo <2854919592@qq.com>
2023-04-05 17:56:15 +0800

    feat(frontend) : top navigation

--- frontend/src/dash_board/test/dash_board.tsx
+++ frontend/src/dash_board/test/dash_board.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { GithubOutlined } from '@ant-design/icons';
-import { Space, Input } from 'antd';
+import { GithubOutlined, AndroidOutlined } from '@ant-design/icons';
+import { Space, Input, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
-import { Header } from 'antd/es/layout/layout';
+import { Header, Content } from 'antd/es/layout/layout';
 
 import './dash_board.css'
 import slash from '../../attachment/img/slash.png'
@@ -20,18 +20,39 @@ export default function TestDashboard(){
 
     return (
         <div>
-            <Header className='header' style={{color:"#002766", height:60}}>
+            <Header className='header'>
                 <Space className='space-container' style={{width:'100%', height:'100%'}}>
-                    <GithubOutlined style={{color:'white', fontSize:24}}/>
-                    <Input style={{width:'100%'}} className="search-input" 
-                        addonBefore={null} placeholder="请输入"  
-                        suffix={suffix} allowClear />
+                    <GithubOutlined className="logo" style={{color:'white', fontSize:24, height:'100%'}}/>
+                    <div className='search-container'>
+                        <Input style={{width:'100%'}} className="search-input" 
+                            addonBefore={null} placeholder="请输入"  
+                            suffix={suffix} allowClear />
+                    </div>
+                    
                     <div className='nav' >
                         <Link to='/hhh'>文档搜索</Link>
                         <Link to='/aaa'>文档分类</Link>
                     </div>
                 </Space>
+                <div className='avatar-container'>
+                    <Avatar className='user-avatar' icon={<AndroidOutlined/>}/>
+                </div>
             </Header>
+
+            <Content className='content-container'>
+                <div className='left'>
+                    <p>Navigator</p>
+                </div>
+                <div className='divider-left'></div>
+                <div className='center'>
+                    <p>Article Content</p>
+                </div>
+                <div className='divider-right'></div>
+                <div className='right'>
+                    <p>Related Articles</p>
+                </div>
+            </Content>
+
         </div>
     )
 }
