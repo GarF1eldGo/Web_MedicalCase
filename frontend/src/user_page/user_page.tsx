72f5987ccd3289e754af0cde9907a3fe7597c4fb
GarF1eldGo <2854919592@qq.com>
2023-04-14 12:14:48 +0800

    feat(frontend) : calendar heatmap

--- frontend/src/user_page/user_page.tsx
+++ frontend/src/user_page/user_page.tsx
@@ -12,6 +12,7 @@ import 'react-calendar-heatmap/dist/styles.css';
 import './user_page.css';
 import FrameWork from '../dash_board/test/framework';
 import userAvatar from '../attachment/img/avatar.jpg';
+import ReactTooltip from 'react-tooltip';
 
 export default function UserPage(){
     const history = useHistory();
@@ -27,17 +28,52 @@ export default function UserPage(){
       { date: new Date(now.getFullYear(), now.getMonth(), 27), count: 1 },
     ];
 
+    function colorScale(value: any) {
+        if(!value){
+            return 'color-empty';
+        }
+        let count = 1;
+        if(value.count < 3){
+            count = 1;
+        }else if(value.count < 5){
+            count = 2;
+        }else if(value.count < 7){
+            count = 3;
+        }else{
+            count = 4;
+        }
+        return `color-scale-${count}`;
+    }
+
+    function heatmapHover(value: any){
+        const opacity = value ? value.count / 4 : 0;
+        if(!value || !value.date){
+            return null;
+        }
+        console.log('value: ', value);
+        return {
+            'data-tip': `${value.date.toDateString()}: ${value.count} article`,
+            style: {
+                opacity: opacity,
+                backgroundColor: `rgba(0, 151, 230, ${opacity})`,
+              },
+        }
+    }
+
     function Overview(){
-        
         return (
             <div className='overview-container'>
-                <div className='overview-hint'>Reading History Count</div>
+                <h3 className='overview-hint'>Read 152 books in the past year</h3>
                 <CalendarHeatmap
                     startDate={new Date(now.getFullYear(), now.getMonth() - 11, 1)}
                     endDate={new Date(now.getFullYear(), now.getMonth() + 1, 0)}
                     values={data}
                     showWeekdayLabels={true}
+                    tooltipDataAttrs={(value:any) => heatmapHover(value)}
+                    classForValue={(value) => colorScale(value)}
+                    onClick={(value) => console.log('click value: ', value)}
                 />
+                <ReactTooltip />
             </div>
         )
     }
